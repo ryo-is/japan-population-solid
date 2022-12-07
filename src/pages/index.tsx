e7f577e08f4bcfac43db9640d8b16a6b45f81ec8
ryo-is <ryosuke.izumi62@gmail.com>
2022-12-07 16:40:46 +0900

    feat: add clear all button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,21 @@ export const TogPage: Component = () => {
   const [selectedIds, setSelectedIds] = createSignal<number[]>([]);
   const [populations] = createResource(selectedIds, fetchPopulation);
 
+  const clearAllSelectedIds = () => {
+    setSelectedIds([]);
+  };
+
   return (
     <>
       <Show when={!prefectures.loading} fallback={<div>loading...</div>}>
         <div class="flex space-x-4">
           <div class="w-1/5">
-            <div class="mb-2">Prefectures</div>
+            <div class="mb-4 flex justify-between items-center">
+              <div>Prefectures</div>
+              <div class="btn btn-primary" onClick={clearAllSelectedIds}>
+                Clear All
+              </div>
+            </div>
             <CheckboxList
               listItems={prefectures()?.result || []}
               selectedIds={selectedIds}
